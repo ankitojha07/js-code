f2d411c6a8f2adfccb62eeba600ea0e900274c1f
Ankit Ojha <ankitojha1409@gmail.com>
2023-12-31 04:18:11 +0530

    data types and Ecma Sript

--- basic_javascript.js
+++ basic_javascript.js
@@ -1,5 +1,25 @@
 "use strict"; // treat all JS code as newer version
 
+// ECMA Standard : https://tc39.es/ecma262/
+
 // alert(3+3) we are using nodejs, not browser
 
 console.log(3+3);
+
+// variables
+let a = 5;
+const b = 6;
+var s = 5;
+
+// datat type in js
+// number
+// string
+// boolean
+// null -> StandAlone data type
+// undefined =>
+// symbol -> unique
+// 
+
+// object
+
+console.log(typeof(a));
